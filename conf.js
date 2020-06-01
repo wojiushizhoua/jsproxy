949db6ae7a682804e1331549447097c4a4bdfa95
wojiushizhoua <49104062+wojiushizhoua@users.noreply.github.com>
2020-06-01 17:17:00 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -15,7 +15,7 @@ jsproxy_config({
     'mysite': {
       label: '当前站点',
       lines: {
-        jsproxy.wojiushizhoua.workers.dev: 1,
+        [location.host]: 1,
       }
     },
     // 该节点用于加载大体积的静态资源
@@ -25,7 +25,7 @@ jsproxy_config({
       lines: {
         // 收费版（高权重）
         //'node-cfworker-2.etherdream.com': 4,
-        
+        'jsproxy.wojiushizhoua.workers.dev': 1,
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
         //'b.007.workers.dev': 1,
